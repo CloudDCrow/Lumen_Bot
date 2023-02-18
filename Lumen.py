1929ff80bb58ed28541671a7436dc420beb2d5eb
CloudDCrow <roki.robic88@gmail.com>
2023-02-18 19:33:50 +0100

    Add text to speech

--- Lumen.py
+++ Lumen.py
@@ -1,10 +1,13 @@
 import openai
 import random
+import pyttsx3
 
 openai.api_key = ""
 model = "text-davinci-003"
 rng = random.randint(0, 5)
 
+engine = pyttsx3.init()
+engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
 greetings = ["Hey. What is on your mind?", "Good Day! How may I be of assistance", "Wats up?",
              "Yo", "Hello, how are you today?", "Hello there. How can I help you today?"]
 goodbyes = ["Until next time", "Sayonara", "Glad to be of service", "Goodbye", "See you", "Bye"]
@@ -20,14 +23,20 @@ def main():
 def run():
     try:
         openai.api_key = input("API Key: ")
+        engine.say(greetings[rng])
+        engine.runAndWait()
         question = input(greetings[rng])
         while True:
             if question in exit_inputs:
                 print(goodbyes[rng])
+                engine.say(goodbyes[rng])
+                engine.runAndWait()
                 break
             else:
                 (res, usage) = request(question)
                 print(res)
+                engine.say(res)
+                engine.runAndWait()
                 print("Tokens used: " + str(usage))
                 question = input()
 
@@ -39,8 +48,8 @@ def request(question):
     response = openai.Completion.create(
         engine=model,
         prompt=question,
-        max_tokens=500,
-        temperature=0.6,
+        max_tokens=100,
+        temperature=1.0,
     )
     return str.strip(response['choices'][0]['text']), response['usage']['total_tokens']
 
