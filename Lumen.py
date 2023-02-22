1df2861a9f19723b4b90f40a46dfa417235b9534
CloudDCrow <roki.robic88@gmail.com>
2023-02-22 18:27:58 +0100

    Add speech recognition

--- Lumen.py
+++ Lumen.py
@@ -1,6 +1,7 @@
 import openai
 import random
 import pyttsx3
+import speech_recognition as sr
 
 openai.api_key = ""
 model = "text-davinci-003"
@@ -10,8 +11,10 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 rate = engine.getProperty('rate')
 engine.setProperty('voice', voices[2].id)
-engine.setProperty('rate', 220)
+engine.setProperty('rate', 190)
 
+speech = sr.Recognizer()
+mic = sr.Microphone(device_index=1)
 
 greetings = ["Hey", "Good Day! How may I be of assistance", "What's up?",
              "Yo bro", "Hello, how are you today?", "Hello there. How can I help you today?"]
@@ -29,20 +32,31 @@ def run():
         openai.api_key = input("API Key: ")
         engine.say(greetings[rng])
         engine.runAndWait()
-        question = input(greetings[rng])
         while True:
-            if question in exit_inputs:
+            with mic as source:
+                speech.adjust_for_ambient_noise(source, duration=0.8)
+                print("listening")
+                audio = speech.listen(source)
+                print("done listening")
+
+            try:
+                question = speech.recognize_google(audio)
+            except (sr.UnknownValueError, sr.RequestError) as e:
+                print("Error: {0}".format(e))
+                continue
+
+            if question.lower() in [x.lower() for x in exit_inputs]:
                 print(goodbyes[rng])
+                print("Exiting program now")
                 engine.say(goodbyes[rng])
                 engine.runAndWait()
                 break
             else:
-                (res, usage) = request(question, initial_request)
+                (res, usage) = request("\n" + question, initial_request)
                 print(res)
                 engine.say(res)
                 engine.runAndWait()
                 print("Tokens used: " + str(usage))
-                question = input()
 
     except KeyboardInterrupt:
         print("Exiting Program")
@@ -53,7 +67,7 @@ def request(question, init):
     response = openai.Completion.create(
         engine=model,
         prompt=prompt,
-        max_tokens=500,
+        max_tokens=100,
         temperature=1.0,
     )
     return str.strip(response['choices'][0]['text']), response['usage']['total_tokens']
